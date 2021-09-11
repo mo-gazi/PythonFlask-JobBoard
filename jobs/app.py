e5da8112eeb9cce628be6b3d0e3f9cf397e6f714
Gazimagomed Rasulov <gazimagomed.rasulov@metoffice.gov.uk>
2021-09-11 21:13:19 +0100

    module 4

--- jobs/app.py
+++ jobs/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-    if connection == None:
+    if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
@@ -36,4 +36,6 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
